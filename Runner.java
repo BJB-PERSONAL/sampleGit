cf833caf9cc68e61e115c154315b94c283516d41
Bijo Jacob <bijo.jacob@rntbci.com>
2019-05-17 18:55:51 +0530

    changes in Runner

--- Runner.java
+++ Runner.java
@@ -4,5 +4,13 @@ class Runner{
   public void greet3(){System.out.println("TEST .......");}  
   public static void main(String ...a){
     System.out.println("sample file is show here !!!:)");
+	BiPredicate<String,String> predicateFn = (firstName,lastName)-> firstName.length() > lastName.length();
+	predicateFn.apply("BIJO","JACOB");
+
+	BiPredicate<String,String> predicateFn = (firstName,lastName)-> firstName.length() > lastName.length();
+	predicateFn.apply("BIJO","JACOB");
+
+	BiPredicate<String,String> predicateFn = (firstName,lastName)-> firstName.length() > lastName.length();
+	predicateFn.apply("BIJO","JACOB");
   }  
 }
