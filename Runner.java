7a3d6c934adfa2602121dbab467fe79de63d0d34
Bijo Jacob <bijo.jacob@rntbci.com>
2019-05-17 19:07:17 +0530

    Revert "changes in Runner"
    This reverts commit cf833caf9cc68e61e115c154315b94c283516d41.

--- Runner.java
+++ Runner.java
@@ -1,8 +1,4 @@
 class Runner{
-  public void greet1(){System.out.println("TEST .......");}	
-  public void greet2(){System.out.println("TEST .......");}
-  public void greet3(){System.out.println("TEST .......");}  
   public static void main(String ...a){
     System.out.println("sample file is show here !!!:)");
-  }  
 }
