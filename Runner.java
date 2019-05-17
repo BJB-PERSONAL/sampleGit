54a700f78477f6b5156f6acc2a3547dd6af23a57
Bijo Jacob <bijo.jacob@rntbci.com>
2019-05-17 17:39:48 +0530

    4th commit

--- Runner.java
+++ Runner.java
@@ -1,6 +1,7 @@
 class Runner{
-
-  public void greet(){System.out.println("TEST .......");}	
+  public void greet1(){System.out.println("TEST .......");}	
+  public void greet2(){System.out.println("TEST .......");}
+  public void greet3(){System.out.println("TEST .......");}  
   public static void main(String ...a){
     System.out.println("sample file is show here !!!:)");
   }  
