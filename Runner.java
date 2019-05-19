4bc3342c6535255f46527df623ee223c73354888
BIJO JACOB <bijo.jacob@rntbci.com>
2019-05-19 08:21:37 +0530

    4th commit

--- Runner.java
+++ Runner.java
@@ -2,8 +2,5 @@ class Runner{
   public static void main(String ...a){
     System.out.println("sample file is show here");
 	System.out.println("ABCD)");
-
-	System.out.println("BBBB)");
-	  System.out.println("BCEF)");
-	  
+	System.out.println("XYZ)"); 
 }
